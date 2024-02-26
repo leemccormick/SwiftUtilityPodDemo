bb96082eca89d3f5868d771caabcca57e41dfe7e
Lee McCormick <lee.mccormick@dialexa.com>
2024-02-26 15:45:27 -0600

    Added validation value

--- SwiftUtilityPodDemo/Validation.swift
+++ SwiftUtilityPodDemo/Validation.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-public struct Validation {
+public class Validation {
+    public let validationGreeting  = "Hello, I am a validation demo."
+    public init() { }
     public func isValidEmail(_ email: String) -> Bool {
         // Regular expression pattern to match email format
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
