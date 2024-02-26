46b8f548e1587c964559758581c52ef24a5e8abe
Lee McCormick <lee.mccormick@dialexa.com>
2024-02-26 13:41:05 -0600

    Initial commit

---
+++ SwiftUtilityPodDemo/SwiftUtilityPodDemo.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUtilityPodDemo.h
+//  SwiftUtilityPodDemo
+//
+//  Created by Lee McCormick on 2/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUtilityPodDemo.
+FOUNDATION_EXPORT double SwiftUtilityPodDemoVersionNumber;
+
+//! Project version string for SwiftUtilityPodDemo.
+FOUNDATION_EXPORT const unsigned char SwiftUtilityPodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtilityPodDemo/PublicHeader.h>
+
+
---
+++ SwiftUtilityPodDemo/Validation.swift
@@ -0,0 +1,20 @@
+//
+//  Validation.swift
+//  SwiftUtilityPodDemo
+//
+//  Created by Lee McCormick on 2/26/24.
+//
+
+import Foundation
+
+public struct Validation {
+    public func isValidEmail(_ email: String) -> Bool {
+        // Regular expression pattern to match email format
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        // NSPredicate to evaluate the email format
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        
+        return emailPredicate.evaluate(with: email)
+    }
+}
