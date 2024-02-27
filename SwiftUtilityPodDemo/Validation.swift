726daf4b87d9c37debb7478fc66a0ded653b6124
Lee McCormick <lee.mccormick@dialexa.com>
2024-02-27 11:28:08 -0600

    added validate date

--- SwiftUtilityPodDemo/Validation.swift
+++ SwiftUtilityPodDemo/Validation.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 public class Validation {
-    public let validationGreeting  = "Hello, I am a validation demo."
     public init() { }
+    
+    public let validationGreeting  = "Hello, I am a validation demo."
+    
     public func isValidEmail(_ email: String) -> Bool {
         // Regular expression pattern to match email format
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
@@ -18,5 +20,11 @@ public class Validation {
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         
         return emailPredicate.evaluate(with: email)
+    }
+    
+    public func isValidDateString(_ dateString: String, format: String = "yyyy-MM-dd") -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.date(from: dateString) != nil
     }
 }
