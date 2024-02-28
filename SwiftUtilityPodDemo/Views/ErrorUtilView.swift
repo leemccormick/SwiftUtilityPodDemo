ecfb7df9289be207e4235c6b09659781c8551739
Lee McCormick <lee.mccormick@dialexa.com>
2024-02-28 13:08:32 -0600

    Added 2 Views for demo and update podspec version to 0.0.7
    - added ErrorView and Loading View
    -  Changed spec.version      = "0.0.6" to spec.version      = "0.0.7"

---
+++ SwiftUtilityPodDemo/Views/ErrorUtilView.swift
@@ -0,0 +1,47 @@
+//
+//  ErrorView.swift
+//  SwiftUtilityPodDemo
+//
+//  Created by Lee McCormick on 2/28/24.
+//
+
+import SwiftUI
+
+public struct ErrorUtilView: View {
+    private let errorMessage: String
+    
+    public init(errorMessage: String) {
+        self.errorMessage = errorMessage
+    }
+    
+    public var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.red).opacity(0.8)
+                
+                Text("Error !")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+                
+                Spacer()
+                    .frame(maxWidth: .infinity)
+            }
+            
+            Text("\(errorMessage)")
+                .font(.caption)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding()
+        .background(Color.red.opacity(0.1))
+        .border(.red, width: 3)
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    ErrorUtilView(errorMessage: "Something wrong, please help Something wrong, please help kfjasdjfklasjdfkljassadfl;ksadf;lkasl;dfkls;dkf...")
+}
---
+++ SwiftUtilityPodDemo/Views/ThreeDotsLoadingView.swift
@@ -0,0 +1,54 @@
+//
+//  LoadingView.swift
+//  SwiftUtilityPodDemo
+//
+//  Created by Lee McCormick on 2/28/24.
+//
+
+import SwiftUI
+
+public struct ThreeDotsLoadingView: View {
+    @State private var scale: CGFloat = 0.0
+    let animationDuration: Double = 0.5
+    
+    public init(scale: CGFloat) {
+        self.scale = scale
+    }
+    
+    public var body: some View {
+        HStack(spacing: 4) {
+            Circle()
+                .fill(Color.mint.opacity(0.8))
+                .scaleEffect(scale)
+                .frame(width: 10, height: 10)
+                .padding(.leading, 5)
+            
+            Circle()
+                .fill(Color.mint.opacity(0.5))
+                .scaleEffect(scale + 0.33)
+                .frame(width: 10, height: 10)
+                .padding(.leading, 5)
+            
+            Circle()
+                .fill(Color.mint.opacity(0.3))
+                .scaleEffect(scale + 0.66)
+                .frame(width: 10, height: 10)
+                .padding(.leading, 5)
+            
+            Circle()
+                .fill(Color.mint.opacity(0.1))
+                .scaleEffect(scale + 0.88)
+                .frame(width: 10, height: 10)
+                .padding(.leading, 5)
+        }
+        .onAppear {
+            withAnimation(Animation.easeInOut(duration: animationDuration).repeatForever(autoreverses: true)) {
+                scale = 1.0
+            }
+        }
+    }
+}
+
+#Preview {
+    ThreeDotsLoadingView(scale: 10)
+}
